c8816a88e26288f207aba58ce6179fe623adaac9
jfrieden <jfrieden@speedy5.las.iastate.edu>
2024-09-22 14:40:00 -0500

    initial commit

---
+++ activation_extraction_without_filtering.sh
@@ -0,0 +1,63 @@
+#!/bin/bash -l
+
+conda activate neurox_pip
+
+scriptDir="ConceptX/scripts/"   # path to ConceptX script directory
+inputPath="Data/English-German/"       # path to the directory where sentence files are saved
+encoder_input="input.in"       # Encoder Sentences
+decoder_input="label.out"      # Decoder Sentences
+# model="bert-base-uncased"     # Model that we want to do the extraction for
+# model="google/mt5-base"     # Model that we want to do the extraction for
+model="Salesforce/codet5-base"     # Model that we want to do the extraction for
+model_class="T5ForConditionalGeneration"
+NEUROX_PATH="NeuroX/scripts"
+
+sentence_length=300   # maximum sentence length
+minfreq=0 
+maxfreq=15
+delfreq=10000000 
+layers_of_interest="0,1,3,6,9,12" # define layers of interest 
+layers_of_interest="9" # define layers of interest 
+
+
+#specify according to where you want your activations to be saved. Keep a clear structure
+
+encoder_working_file=$encoder_input.tok.sent_len
+decoder_working_file=$decoder_input.tok.sent_len
+
+cp ${inputPath}/$encoder_input $encoder_input.tok
+cp ${inputPath}/$decoder_input $decoder_input.tok
+
+
+# Do sentence length filtering and keep sentences max length of {sentence_length}
+python "code/parallel_sentence_length.py" --encoder_input $encoder_input.tok --decoder_input $decoder_input.tok --encoder_output_file $encoder_working_file --decoder_output_file  $decoder_working_file --length ${sentence_length}
+
+
+# Calculate vocabulary size
+python ${scriptDir}/frequency_count.py --input-file ${encoder_working_file} --output-file ${encoder_working_file}.words_freq
+python ${scriptDir}/frequency_count.py --input-file ${decoder_working_file} --output-file ${decoder_working_file}.words_freq
+
+
+# Extract layer-wise activations
+PYTHONPATH=$NEUROX_PATH python3 -u NeuroX/neurox/data/extraction/transformers_extractor.py "${model},${model},${model_class}" ${encoder_working_file}  ${decoder_working_file} activations.json --output_type json --seq2seq_component both --decompose_layers --filter_layers "$layers_of_interest"
+# PYTHONPATH=$NEUROX_PATH python3 -u NeuroX/neurox/data/extraction/transformers_extractor.py "${model}" ${encoder_working_file}  ${decoder_working_file} activations.json --output_type json --seq2seq_component both --decompose_layers --filter_layers "$layers_of_interest"
+
+# Create a dataset file with word and sentence indexes
+layers_of_interest="0 1 3 6 9 12"
+
+layers_of_interest="9"
+for j in $layers_of_interest 
+do
+    python ${scriptDir}/create_data_single_layer.py --text-file ${encoder_working_file} --activation-file encoder-activations-layer${j}.json --output-prefix "${encoder_working_file}_${j}" 
+    python ${scriptDir}/create_data_single_layer.py --text-file ${decoder_working_file} --activation-file decoder-activations-layer${j}.json --output-prefix "${decoder_working_file}_${j}"
+    python ${scriptDir}/frequency_filter_data.py --input-file ${encoder_working_file}_${j}-dataset.json --frequency-file ${encoder_working_file}.words_freq --sentence-file "${encoder_working_file}_${j}-sentences.json" --minimum-frequency $minfreq --maximum-frequency $maxfreq --delete-frequency ${delfreq} --output-file "${encoder_working_file}_${j}"
+
+    python ${scriptDir}/frequency_filter_data.py --input-file "${decoder_working_file}_${j}-dataset.json" --frequency-file "${decoder_working_file}.words_freq" --sentence-file "${decoder_working_file}_${j}-sentences.json" --minimum-frequency $minfreq --maximum-frequency $maxfreq --delete-frequency ${delfreq} --output-file "${decoder_working_file}_${j}"
+    python -u ${scriptDir}/extract_data.py --input-file ${encoder_working_file}_${j}_min_${minfreq}_max_${maxfreq}_del_${delfreq}-dataset.json --output-vocab-file encoder-processed-vocab-${j}.npy --output-point-file encoder-processed-point-${j}.npy
+    python -u ${scriptDir}/extract_data.py --input-file ${decoder_working_file}_${j}_min_${minfreq}_max_${maxfreq}_del_${delfreq}-dataset.json --output-vocab-file decoder-processed-vocab-${j}.npy --output-point-file decoder-processed-point-${j}.npy
+
+done
+
+rm -r *-dataset.json
+rm -r *-labels.json
+rm -r *activations*.json
---
+++ clustering.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -l
+
+conda activate neurox_pip
+vocab_file="encoder-processed-vocab-9.npy" # specify the path to the vocab file from the activation extraction step
+point_file="encoder-processed-point-9.npy" # specify the path to the point file from the activation extraction step
+output_path="cluster-output" # specify the path to the output file 
+
+clusters=3 # specify the number of clusters
+
+python -u code/create_kmeans_clustering.py -v $vocab_file -p $point_file -o $output_path -k $clusters
