64a607df4e5bc9f39e3b7559bc820f5ebcb1cff1
Vedant Pungliya <vedant29@iastate.edu>
2025-01-19 20:50:18 -0600

    Added awesome-align as regular files and updated CPP-Cuda data files

---
+++ awesome-align/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import codecs
+
+setup(
+    name='awesome_align',
+    install_requires=[
+        'tokenizers>=0.5.2',
+        'torch>=1.2.0',
+        'tqdm',
+        'numpy',
+        'boto3',
+        'filelock',
+        'requests',
+        'transformers>=4.0.0',
+        'urllib3<1.27,>=1.25.4',
+        'huggingface-hub>=0.0.12'
+    ],
+    version='0.2.0',
+    author='NeuLab',
+    author_email='zdou0830@gmail.com',
+    license='BSD 3-Clause',
+    description='An awesome word alignment tool',
+    long_description=codecs.open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "awesome-align=awesome_align.run_align:main",
+            "awesome-train=awesome_align.run_train:main",
+        ],
+    },
+    url='https://github.com/neulab/awesome-align',
+    python_requires='>=3.6',
+    include_package_data=True,
+    zip_safe=False,
+)
